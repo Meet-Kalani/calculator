1878d593f872c117f3db6e4e32494f6b825849e7
Meetkalani <meetkalani2002@gmail.com>
2024-02-20 00:28:51 +0530

    implement keyboard access

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,3 @@
-// . point value add this functionality
 const display = document.querySelector('.display-number');
 const numberElements = document.querySelectorAll('.number');
 const operatorElements = document.querySelectorAll('.operator');
@@ -18,9 +17,27 @@ equalsElement.addEventListener('click', calculate);
 
 backspaceElement.addEventListener('click', backspace);
 
+// check this case 102.30.2
 display.addEventListener('keypress', function (event) {
     const key = event.key;
-    
+    let isPointUsed = false;
+    if(key === ".") {
+        isPointUsed = true;
+    }
+
+    let allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/','.'];
+
+    console.log(isPointUsed && key === '.');
+    console.log(!allowedKeys.includes(key) || isPointUsed && key === '.') 
+    if (!allowedKeys.includes(key)) {
+        // Prevent the input if the key is not allowed
+        event.preventDefault();
+    }
+
+    if (allowedKeys.includes(key) && (display.value === "Error!" || display.value === "Infinity")) {
+        display.value = "";
+    }
+
     if (key === 'Enter') {
         calculate();
     }
@@ -32,9 +49,10 @@ function clearDisplay() {
 }
 
 function calculate() {
-    try{
+    try {
         display.value = eval(display.value);
-    } catch(err){
+    } catch (err) {
+        // check thiscase it should not be log error to console but handle it properly
         console.error(err);
         display.value = "Error!";
     }
@@ -45,10 +63,10 @@ function backspace() {
 }
 
 function appendToDisplay(value) {
-    if(display.value === "Error!" || display.value === "Infinity"){
+    if (display.value === "Error!" || display.value === "Infinity") {
         display.value = "";
     } else if (['+', '-', '*', '/'].includes(display.value.slice(-1)) && ['+', '-', '*', '/'].includes(value)) {
         return;
     }
-    display.value += value;  
+    display.value += value;
 }
